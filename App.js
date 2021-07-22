b00f2b7d10e3fdd611e43ddc68d96026f96f0f31
Felipe Alves <felipeaamodio@gmail.com>
2021-07-22 11:46:03 -0300

    configurando o feed

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Header from './src/Header';
+import List from './src/List';
 
 export default function App() {
+  const [feed, setFeed] = useState([
+    {
+      id: '1',
+      nome: 'Felipe Alves',
+      desc: 'Mais um dia de muitos bugs',
+      imgPerfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png',
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
+      liked: true, 
+      likers: 1
+    },
+    {
+      id: '2', 
+      nome: 'Matheus Fraga', 
+      descricao: 'Isso sim é ser raiz!!!!!', 
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
+      liked: false, 
+      likers: 0
+    },
+    {
+      id: '3', 
+      nome: 'Jose Augusto', 
+      descricao: 'Bora trabalhar, hoje estou começando em um projeto novo aqui no sujeito, desde o backend ao frontend', 
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
+      liked: false, 
+      likers: 3
+    },
+    {
+      id: '4', 
+      nome: 'Gustavo Henrique', 
+      descricao: 'Isso sim que é TI!', 
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
+      liked: false, 
+      likers: 1
+    },
+    {
+      id: '5', 
+      nome: 'Guilherme Luis', 
+      descricao: 'Boa tarde galera do insta...', 
+      imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+      imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+      liked: false, 
+      likers: 32
+    }
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList 
+        data={feed}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <List data={item} />}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
