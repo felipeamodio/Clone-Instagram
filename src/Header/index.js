09f41f09f3a5b8ec2782a685ad9f3654fb02f7c9
Felipe Alves <felipeaamodio@gmail.com>
2021-07-22 11:31:22 -0300

    finzalizando a header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
 export default function Header(){
     return(
         <View style={styles.header}>
-            <Text>Meu Header</Text>
+            <TouchableOpacity>
+                <Image 
+                    source={require('../img/logo.png')}
+                    style={styles.logo}
+                />
+            </TouchableOpacity>
+            
+            <TouchableOpacity>
+                <Image 
+                    source={require('../img/send.png')}
+                    style={styles.send}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
@@ -14,6 +26,17 @@ const styles = StyleSheet.create({
         height: 55,
         backgroundColor: '#FFFFFF',
         flexDirection: 'row',
-        
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+        paddingRight: 15,
+        marginTop: 45,
+        borderBottomWidth: 0.2,
+        shadowColor: '#000000',
+        elevation: 1
+    },
+    send: {
+        width: 23,
+        height: 23
     }
 })
