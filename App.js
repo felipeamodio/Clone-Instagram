52fc49800f5f0438ec0fb304bff371e37be41cec
Felipe Alves <felipeaamodio@gmail.com>
2021-07-23 09:20:21 -0300

    updates

--- App.js
+++ App.js
@@ -52,6 +52,7 @@ export default function App() {
         keyExtractor={(item) => item.id}
         renderItem={({item}) => <List data={item} />}
         showsVerticalScrollIndicator={false}
+        style={{marginTop: 10}}
       />
     </View>
   );
@@ -59,6 +60,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
 });
